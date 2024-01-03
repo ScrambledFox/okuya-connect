2e9f50459a561c9e1cc39f39ca0eb3b6e9367ed5
ScrambledFox <github@jorislodewijks.nl>
2024-01-03 14:04:56 +0100

    feat: basic server

---
+++ server.ts
@@ -0,0 +1,24 @@
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+const port = process.env.PORT || 6969;
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+server.listen(port, () => {
+  console.log(`started on port: ${port}`);
+});
